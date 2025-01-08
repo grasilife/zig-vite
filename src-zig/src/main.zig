bd0c7930e01f42d0c8a9ae101d4efde4de7fbad2
付国强 <434543162@qq.com>
2025-01-09 01:47:15 +0800

    优化打印

--- src-zig/src/main.zig
+++ src-zig/src/main.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 const WebView = @import("webview").WebView;
+
 pub fn main() !void {
-    //webview
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // webview
     const w = WebView.create(false, null);
     defer w.destroy();
+    const is_dev: bool = true;
+    const path = if (is_dev) "http://localhost:3020" else "./dist/index.html";
+
+    // 修复这里的格式化字符串
+    std.debug.print("path: {s}\n", .{path}); // 正确传递 path 变量
     w.setTitle("Basic Example");
     w.setSize(480, 320, WebView.WindowSizeHint.None);
     w.setHtml("Thanks for using webview!");
